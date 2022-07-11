6b907cac53cf5bd1c76128faa1654e87c4a303c0
Lauro Oliveira <lauro.oliveira@liv.local>
2022-07-11 16:51:03 -0300

    .

--- trilha-especializar/js-assincrono/callback-02.js
+++ trilha-especializar/js-assincrono/callback-02.js
@@ -1,4 +1,4 @@
-const https = requestIdleCallback('https')
+const https = require('https')
 const API = 'https://jsonplaceholder.typicode.com/users?_limit=2'
 https.get(API, res => {
     console.log(res.statusCode);
